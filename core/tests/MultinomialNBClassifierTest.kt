3c06dee1083014a981969dd7ca29d21ce2226c83
Joseph Catrambone <jo.jcat@gmail.com>
2018-01-11 22:40:26 -0800

    Start building classifiers in the test.  Add a bunch of speeches to grab for ML.

--- core/tests/MultinomialNBClassifierTest.kt
+++ core/tests/MultinomialNBClassifierTest.kt
@@ -8,4 +8,81 @@ class MultinomialNBClassifierTest {
 	fun testNBClassify() {
 		assertTrue(false)
 	}
+
+	//Regulation	vs	Deregulation
+	//You side moderately towards “Regulation”, meaning you believe the government should regulate business activity to ensure no one is taking advantage of the system. This theme is most important to you.
+	@Test
+	fun buildRegulationClassifier() {
+
+	}
+
+	//Left	vs	Right
+	//You side extremely towards “Left”, meaning you very strongly support policies that promote social and economic equality. This theme is most important to you.
+	@Test
+	fun buildLeftRightClassifier() {
+
+	}
+
+	//Religious	vs	Secular
+	//You side strongly towards “Secular”, meaning you strongly support policies that reflect a separation of church and state. This theme is most important to you.
+	@Test
+	fun buildSecularReligiousClassifier() {
+
+	}
+
+	//Small Government	vs	Big Government
+	//You side extremely towards “Big Government”, meaning you very strongly believe the government should do more to address social inequality, corruption, and assistance for its citizens. This theme is more important to you.
+	@Test
+	fun buildBigSmallGovernmentClassifier() {
+
+	}
+
+	//Collectivism	vs	Individualism
+	//You side strongly towards “Collectivism”, meaning you strongly believe policy should protect and support the best interests of all citizens. This theme is more important to you.
+	@Test
+	fun buildCollectivismIndividualismClassifier() {
+
+	}
+
+	//Traditional	vs	Progressive
+	//You side extremely towards “Progressive”, meaning you very strongly believe we should be a nation that values personal freedom, expression, and diversity. This theme is more important to you.
+	fun buildProgressiveTraditionalClassifier() {
+
+	}
+
+	//Laissez-faire	vs	Keynesian
+	//You side extremely towards “Keynesian”, meaning you very strongly believe the government should provide economic assistance to stabilize the economy. This theme is more important to you.
+	fun buildLaissezFaireKeynesianClassifier() {
+
+	}
+
+	//Multiculturalism	vs	Assimilation
+	//You side moderately towards “Multiculturalism”, meaning you believe we should celebrate and embrace cultural diversity. This theme is more important to you.
+	fun buildMulticulturalismAssimulationClassifier() {
+
+	}
+
+	//Tough	vs	Tender
+	//You side extremely towards “Tender”, meaning you very strongly believe in showing compassion, empathy, and rehabilitation for small-time criminals or those struggling with addiction. This theme is somewhat important to you.
+	fun buildTenderToughClassifier() {
+
+	}
+
+	//Privacy	vs	Security
+	//You side moderately towards “Privacy”, meaning you believe the government should not undermine the rights and privacy of its citizens under the guise of national security. This theme is somewhat important to you.
+	fun buildPrivacySecurityClassifier() {
+
+	}
+
+	//Politically Correct	vs	Politically Incorrect
+	//You side slightly towards “Politically Correct”, meaning you more often believe we should not use language or behavior which could offend, marginalize, or insult those who are disadvantaged or discriminated against. This theme is somewhat important to you.
+	fun politicallyCorrectIncorrectClassifier() {
+
+	}
+
+	//Libertarian	vs	Authoritarian
+	//You are a centrist on Libertarian and Authoritarian issues. This theme is more important to you.
+	fun buildLibertarianAuthoritarianClassifier() {
+
+	}
 }
